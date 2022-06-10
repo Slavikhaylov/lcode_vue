02d856ae80f14c74c68b7522f58c8fd3a7c44622
slavikhaylov <hslavik627@gmail.com>
2022-06-10 20:13:17 +0500

    config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
+const setPublicPath = require("/lcode_vue/");
 module.exports = defineConfig({
   transpileDependencies: true
-})
+}),
+setPublicPath
